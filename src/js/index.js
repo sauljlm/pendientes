584fe3946ad743d5bdbc99d032f774fd7340350d
Saul Lopez Molina <saulsjlm2@gmail.com>
2019-11-08 00:05:47 -0600

    cambio formato de icono para PWA

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-import '../img/logo.jpg';
+import '../img/logo.png';
 import '../img/done.png';
 import '../img/delete.png';
 import '../img/sinTareas.png';
